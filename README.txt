a50daee47111547bcc82074424d16273a0a16d24
Atif Siddiqui <asiddi27@depaul.edu>
2023-11-20 22:18:16 -0600

    Fixed some minor stuff and added Powerpoint

---
+++ README.txt
@@ -0,0 +1,5 @@
+Final Project for 450 by Atif Siddiqui - Shopping Cart system
+To run in eclipse:
+Simply import as a Gradle Porject and run the Main.java class located 
+in ShoppingSystem\src\main\java
+Logs are stored/created in the ShoppingSystem folder
--- src/main/java/Products/ProductFactory.java
+++ src/main/java/Products/ProductFactory.java
@@ -8,6 +8,12 @@ public class ProductFactory {
     public Product createJOPrenguinProduct(String name, double price) {
         return new JumboOrnamentalPenguin(name, price);
     }
+    public Product createMiPrenguinProduct(String name, double price) {
+        return new MiniOrnamentalPenguin(name, price);
+    }
+    public Product createHPrenguinProduct(String name, double price) {
+        return new OrnamentalPenguinWithaHat(name, price);
+    }
 
 
 }
